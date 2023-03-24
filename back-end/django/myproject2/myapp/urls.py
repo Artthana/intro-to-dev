f31ede15c307c9300e4855a00c3a5a57aefb1885
Artthana <wongsathorn.thana@gmail.com>
2023-03-24 16:36:29 +0700

    เพิ่มการ include base template และเปลี่ยน setting

--- back-end/django/myproject2/myapp/urls.py
+++ back-end/django/myproject2/myapp/urls.py
@@ -1,10 +1,11 @@
+from os import name
 from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('',views.index),
-    path('about',views.about),
-    path('form',views.form),
-    path('edit/<person_id>',views.edit),
-    path('delete/<person_id>',views.delete)
+    path('',views.index, name='home'),
+    path('about',views.about, name='about'),
+    path('form',views.form, name='form'),
+    path('edit/<person_id>',views.edit, name='edit'),
+    path('delete/<person_id>',views.delete, name='delete')
 ]
